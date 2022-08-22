49260a1fea11d55d920300d3a0abad4dd34718cf
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-22 16:01:04 +0300

    Completed Challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(response => response.json())
+.then(dogs => {
+    dogs.message.forEach(dog =>{
+        const dogImg = document.createElement('img')
+        dogImg.style.height = '30vw'
+        dogImg.style.margin = '1vw'
+
+        dogImg.src = dog;
+        dogImg.alt = 'a dog image'        
+
+        document.getElementById('dog-image-container')
+        .appendChild(dogImg)
+    })
+})
+
